a322ccb100d080d2d86b99a9a245e8a4fe75b87b
irhas-kf <irhas.cepunk@gmail.com>
2020-08-21 17:47:09 +0700

    datasbae

--- ruanginggris1.sql
+++ ruanginggris1.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 21/08/2020 17:46:55
+ Date: 19/08/2020 21:30:55
 */
 
 SET NAMES utf8mb4;
@@ -29,15 +29,12 @@ CREATE TABLE `materi`  (
   `video` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `tanggal_publikasi` datetime(0) NULL DEFAULT NULL,
   PRIMARY KEY (`id_materi`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of materi
 -- ----------------------------
 INSERT INTO `materi` VALUES (1, '1', 'Vocabulary', 'Vocabulary', 'demicinta.mp4', '2020-08-16 23:13:20');
-INSERT INTO `materi` VALUES (11, '1', 'materii', 'kontenn', '1939266585.mp4', '2020-08-21 08:28:01');
-INSERT INTO `materi` VALUES (12, '1', 'asu', 'asu', '1107545861.mp4', '2020-08-21 10:22:31');
-INSERT INTO `materi` VALUES (13, '1', 'asu', 'asu', '1622171225.mp4', '2020-08-21 10:33:56');
 
 -- ----------------------------
 -- Table structure for migrations
@@ -130,7 +127,7 @@ CREATE TABLE `users`  (
 -- ----------------------------
 INSERT INTO `users` VALUES (1, 'fauzin', 'fauzin', 'fauzin@gmail.com', '2020-08-18 23:34:11', '$2y$10$YogCCBcCikQjJwzqFL0UY.jDuEDDU8X3/vyKqCK1irepYEwaYzzme', 'fauzin123', NULL, NULL, NULL, NULL, 'ooJ66v0LS0zQVCiJem2NAPa9y5EfWFxRmywG89dT3QIBFhx1hi2NEXgymzja', '2020-08-18 23:34:11', '2020-08-18 23:34:11', 'admin', NULL, 'Paket 4', '2020-08-18', '2020-09-18');
 INSERT INTO `users` VALUES (2, 'haris', 'haris', 'haris@gmail.com', '2020-08-18 23:26:03', '$2y$10$9UxiDZL6PUbnc.zh1jGKcer043I21bfTQb.xZNsKKChE8Uy7jMqMe', 'haris123', NULL, NULL, '085855042212', NULL, 'FJqGgJVqkyeh68Cxsp3BcetFRXeB3mJFoh55Yq4NxMeYMhXfWrxLJpIO5qSY', '2020-08-18 23:26:03', '2020-08-18 23:26:03', 'peserta', NULL, 'Paket 4', '2020-08-18', '2020-09-18');
-INSERT INTO `users` VALUES (3, 'irhas', 'irhas', 'irhas@gmail.com', '2020-08-20 22:07:49', '$2y$10$lOx3e4YCI3Q2yKqq48Gto.6v6b/d8UAHeAA3pskO3eNCVUv9FWo3m', 'irhas123', NULL, NULL, NULL, NULL, 'wsGYAvV9RgiMIZZJb3YYylql4v6Pq68plOyreUnu5eC7tuFNPmvkKpisk1pp', '2020-08-20 22:07:49', '2020-08-20 22:07:49', 'admin', NULL, 'Paket 4', '2020-08-18', '2020-09-18');
+INSERT INTO `users` VALUES (3, 'irhas', 'irhas', 'irhas@gmail.com', '2020-08-18 23:29:28', '$2y$10$lOx3e4YCI3Q2yKqq48Gto.6v6b/d8UAHeAA3pskO3eNCVUv9FWo3m', 'irhas123', NULL, NULL, NULL, NULL, 'wsGYAvV9RgiMIZZJb3YYylql4v6Pq68plOyreUnu5eC7tuFNPmvkKpisk1pp', '2020-08-18 23:29:28', '2020-08-18 23:29:28', 'peserta', NULL, 'Paket 4', '2020-08-18', '2020-09-18');
 INSERT INTO `users` VALUES (2098, 'Agus Eko Setyono', 'Agus Eko Setyono', NULL, NULL, '$2y$10$X9HkwBEfiACjwkICDbwDUuOC/1NM01icOpFv8/nFHS.UcxQbBj/sa', 'yourpassword', 'Kab. Grobogan', NULL, '6285325108027', 'foto.jpg', NULL, NULL, NULL, 'peserta', '18522416', 'PAKET 4 BUKU JAGO ENGLISH', '2020-08-18', '2020-10-18');
 INSERT INTO `users` VALUES (2099, 'Gilang ramadhan setiadi', 'Gilang ramadhan setiadi', NULL, NULL, '$2y$10$w37qOab/J1QnRhd5MvXQNuKi3ALUrBq3wDCYYK06E4avn4Ajix0xC', 'yourpassword', 'Kota Samarinda', NULL, '6283141786960', 'foto.jpg', NULL, NULL, NULL, 'peserta', '17706078', 'PAKET 4 BUKU JAGO ENGLISH', '2020-08-18', '2020-10-18');
 INSERT INTO `users` VALUES (2100, 'Yanti Kurniasari', 'Yanti Kurniasari', NULL, NULL, '$2y$10$ehO/TfqclyBO5kLbG0V5aeMJ7f9hOMGMIr3AK95UFVHAZ5c9elx0q', 'yourpassword', 'Kota Samarinda', NULL, '6282290329215', 'foto.jpg', NULL, NULL, NULL, 'peserta', '17518532', 'PAKET 4 BUKU JAGO ENGLISH', '2020-08-18', '2020-10-18');
